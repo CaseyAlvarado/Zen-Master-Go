4900b6bec1807a971f6795fd2f9eae21a775cadd
Pratool <pratool.gadtaula@students.olin.edu>
2014-04-14 12:20:28 -0400

    Working flatten tree code added

--- TicTacToe/TicTacToe_OneComputer.py
+++ TicTacToe/TicTacToe_OneComputer.py
@@ -1,4 +1,38 @@
-import random, math
+import random, math, sys
+
+def check_for_list(branch):
+    for item in branch:
+        if type(item) == list:
+            return True
+    return False
+
+def modified_min(in_list):
+    minimum = sys.maxint
+    for element in in_list:
+        if type(element) == tuple:
+            if element[0] < minimum:
+                minimum = element[0]
+        elif element < minimum:
+            minimum = element
+    return minimum
+
+def modified_max(in_list):
+    maximum = -sys.maxint - 1
+    for element in in_list:
+        if type(element) == tuple:
+            if element[0] > maximum:
+                maximum = element[0]
+        elif element > maximum:
+            maximum = element
+    return maximum
+
+def find_max_depth(in_list, current_depth):
+    max_depth = current_depth
+    for element in in_list:
+        if type(element) == tuple:
+            if element[1] > max_depth:
+                max_depth = element[1]
+    return max_depth
 
 class TicTacToe:
     """ Encodes the state of the Tic Tac Toe board """
@@ -92,6 +126,22 @@ class TicTacToe:
                 tree.append(self.generate_tree(temp_board, not comp_turn))
                 
         return tree
+    
+    def flatten_tree(self, rec_tree, depth):
+        if not(check_for_list(rec_tree)):   # if there are no lists in rec_tree
+            if depth % 2 == 0:              # if it's computer's move
+                                            # return a tuple with the max rec_tree
+                                            # value and rec_tree depth
+                return (modified_max(rec_tree), find_max_depth(rec_tree, depth))
+            return (modified_min(rec_tree), find_max_depth(rec_tree, depth))
+        else:
+            branch = []
+            for element in rec_tree:
+                if type(element) == list:
+                    branch.append(self.flatten_tree(element, depth+1))
+                else:
+                    branch.append(element)
+            return self.flatten_tree(branch, depth)
         
     def display_board(self):
         for i in range(3):
@@ -107,6 +157,9 @@ if __name__ == '__main__':
         game.computer_move()
         
     game.display_board()
+    
+    print game.flatten_tree([1, 2, [5, 8, 6], [3, 8, 4], 6], 0)
+    print game.flatten_tree([1, 2, [5, 6, [1, 0, 4], 3]], 0)
         
     while True:
         game.player_move()
